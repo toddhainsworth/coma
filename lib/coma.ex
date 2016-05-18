d0fa31cd83abe2272970070bbdef9491770a5d00
Todd Hainsworth <hainsworth.todd@gmail.com>
2016-05-18 20:47:52 +0930

    add incredibly minimal writing

--- lib/coma.ex
+++ lib/coma.ex
@@ -4,19 +4,42 @@ defmodule Coma do
   """
 
   @doc """
-  Reads and parses the file identified by the given
-  filename if it exists.
+  Reads and parses the file identified by the given filename
   """
   def read(filename) do
-    if File.exists?(filename) do
-      case File.read(filename) do
-        { :ok, contents } -> { :ok, parse_csv contents }
-        error -> error
-      end
-    else
-      { :error, "File not found" }
+    case File.read(filename) do
+      { :ok, contents } -> { :ok, parse_csv contents }
+      error -> error
     end
   end
+
+  @doc """
+  Writes the row to the file identified by the given filename based
+  on the given mode (defaults to :append).
+
+  ## Modes
+
+  The available modes include:
+  - :append (Default, append to the end of the file)
+  - :overwrite (Overwrite the contents of the file with the given row)
+  """
+  def write_row(row, filename, mode \\ :append) do
+    csv = Enum.join(row, ",") <> "\n"
+    write_mode = case mode do
+      :append -> :append
+      :overwrite -> :write
+      _ -> :write
+    end
+
+    # TODO: Better to use File.open/2 as per the docs for File.write/3
+    # Because if this is ever used within a loop (likely) we will get decreased
+    # performance
+    case File.write(filename, csv, [write_mode]) do
+      :ok   -> { :ok , row }
+      error -> error
+    end
+  end
+
 
   defp parse_csv(contents) do
     String.split(contents, "\n")              # Split into rows
--- test/coma_test.exs
+++ test/coma_test.exs
@@ -1,10 +1,6 @@
 defmodule ComaTest do
   use ExUnit.Case
 
-  test "read/1 when file does not exist" do
-    assert Coma.read("does_not_exist") == { :error, "File not found" }
-  end
-
   test "read/1 when file does exist and does not contain any text" do
     assert Coma.read("empty_csv.csv") == { :ok, [] }
   end
@@ -22,5 +18,65 @@ defmodule ComaTest do
 
   test "read/1 when file does exist but is not a valid csv file" do
     assert Coma.read("not_valid.csv") == { :ok, [["the contents"]] }
+  end
+
+  test "write_row/3 when all arguments are valid" do
+    assert Coma.write_row(["test", "this", "out"], "my_csv.csv", :overwrite)
+      == { :ok, ["test", "this", "out"] }
+
+    assert File.read("my_csv.csv") == { :ok, "test,this,out\n" }
+  end
+
+  test "write_row/3 when the file does not exist" do
+    assert File.exists?("does_not_exist") == false
+    assert Coma.write_row(["test", "this", "out"], "does_not_exist", :overwrite)
+      == { :ok, ["test", "this", "out"] }
+    assert File.exists?("does_not_exist") == true
+    # Cleanup
+    File.rm("does_not_exist")
+  end
+
+  test "write_row/3 when mode is :append" do
+    Coma.write_row(["test", "this", "out"], "append_to_this.csv", :append)
+    Coma.write_row(["foo", "the", "bar"], "append_to_this.csv", :append)
+
+    { _, contents } = File.read("append_to_this.csv")
+    assert contents == "test,this,out\nfoo,the,bar\n"
+
+    # Cleanup
+    File.rm("append_to_this.csv")
+  end
+
+  test "write_row/3 when mode is :overwrite" do
+    Coma.write_row(["test", "this", "out"], "overwrite_this.csv", :overwrite)
+    Coma.write_row(["foo", "the", "bar"], "overwrite_this.csv", :overwrite)
+
+    { _, contents } = File.read("overwrite_this.csv")
+    assert contents == "foo,the,bar\n"
+
+    # Cleanup
+    File.rm("overwrite_this.csv")
+  end
+
+  test "write_row/3 when mode is not included" do
+    Coma.write_row(["test", "this", "out"], "append_to_this.csv")
+    Coma.write_row(["foo", "the", "bar"], "append_to_this.csv")
+
+    { _, contents } = File.read("append_to_this.csv")
+    assert contents == "test,this,out\nfoo,the,bar\n"
+
+    # Cleanup
+    File.rm("append_to_this.csv")
+  end
+
+  test "write_row/3 when mode is not a supported type" do
+    Coma.write_row(["test", "this", "out"], "append_to_this.csv", :foo)
+    Coma.write_row(["foo", "the", "bar"], "append_to_this.csv", :foo)
+
+    { _, contents } = File.read("append_to_this.csv")
+    assert contents == "foo,the,bar\n"
+
+    # Cleanup
+    File.rm("append_to_this.csv")
   end
 end
